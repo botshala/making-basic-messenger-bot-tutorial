dce9b0f29927ef8459bd61c64033001a0d44fc29
= <=>
2016-08-06 17:34:03 +0530

    foo

--- fb_chatbot/urls.py
+++ fb_chatbot/urls.py
@@ -7,4 +7,4 @@ urlpatterns = patterns('',
 	url(r'^$', views.index,name = 'index'),
 	url(r'^hi/$', views.hello,name = 'hello'),
 	url(r'^hello/hi/$', views.hello,name = 'hello'),
-	url(r'^facebook_auth/$', MyQuoteBotView.as_view()))
+	url(r'^facebook_auth/?$', MyQuoteBotView.as_view()))
