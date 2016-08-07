be4ea485f4b656bdc62af41073bb8875b5bb0294
Saurav Tomar <stomatrix@gmail.com>
2016-08-07 13:27:41 +0530

    foo

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -19,12 +19,57 @@ VERIFY_TOKEN = '8447789934m'
 quotes_arr = [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
 ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
 ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+["Every strike brings me closer to the next home run.", "Babe Ruth"],
+["Definiteness of purpose is the starting point of all achievement.", "W. Clement Stone"],
+["We must balance conspicuous consumption with conscious capitalism.", "Kevin Kruse"],
+["Life is what happens to you while you’re busy making other plans.", "John Lennon"],
+["We become what we think about.", "Earl Nightingale"],
+["14.Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.", "Mark Twain"],
+["15.Life is 10% what happens to me and 90% of how I react to it.", "Charles Swindoll"],
+["The most common way people give up their power is by thinking they don’t have any.", "Alice Walker"],
+["The mind is everything. What you think you become.", "Buddha"],
+["The best time to plant a tree was 20 years ago. The second best time is now.", "Chinese Proverb"],
+["An unexamined life is not worth living.", "Socrates"],
+["Every child is an artist.  The problem is how to remain an artist once he grows up.", "Pablo Picasso"],
+["You can never cross the ocean until you have the courage to lose sight of the shore.", "Christopher Columbus"],
+["I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou"],
+["Either you run the day, or the day runs you.", "Jim Rohn"],
+["Whether you think you can or you think you can’t, you’re right.", "Henry Ford"],
+["The two most important days in your life are the day you are born and the day you find out why.", "Mark Twain"],
+["Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it.", "Johann Wolfgang von Goethe"],
+["The best revenge is massive success.", "Frank Sinatra"],
+["People often say that motivation doesn’t last. Well, neither does bathing.  That’s why we recommend it daily.", "Zig Ziglar"],
+["Life shrinks or expands in proportion to one’s courage.", "Anais Nin"],
+["If you hear a voice within you say “you cannot paint,” then by all means paint and that voice will be silenced.", "Vincent Van Gogh"],
+["There is only one way to avoid criticism: do nothing, say nothing, and be nothing.", "Aristotle"],
+["Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.", "Jesus"],
+["The only person you are destined to become is the person you decide to be.", "Ralph Waldo Emerson"],
+["Go confidently in the direction of your dreams.  Live the life you have imagined.", "Henry David Thoreau"],
+["When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.", "Erma Bombeck"],
+["Few things can help an individual more than to place responsibility on him, and to let him know that you trust him.", "Booker T. Washington"],
+["Certain things catch your eye, but pursue only those that capture the heart.", " Ancient Indian Proverb"],
+["Believe you can and you’re halfway there.", "Theodore Roosevelt"],
+["Everything you’ve ever wanted is on the other side of fear.", "George Addair"],
+["We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.", "Plato"],
+["Teach thy tongue to say, “I do not know,” and thous shalt progress.", "Maimonides"],
+["Start where you are. Use what you have.  Do what you can.", "Arthur Ashe"],
+["When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.", "John Lennon"],
+["Fall seven times and stand up eight.", "Japanese Proverb"],
 ]
 
 
 def return_random_quote():
     random.shuffle(quotes_arr)
     return quotes_arr[0]
+
+def quote_search(str_var):
+    str_var.lower()
+    random.shuffle(quotes_arr)
+    for quote_text,quote_author in quotes_arr:
+        if str_var in quote_author.lower():
+            return quote_text
+
+    return return_random_quote()
 
 
 def post_facebook_message(fbid, recevied_message):
@@ -38,8 +83,7 @@ def post_facebook_message(fbid, recevied_message):
     except:
         joke_text = 'Yo ' + reply_text
 
-    random_quote = return_random_quote()
-    joke_text = random_quote[0]
+    joke_text = quote_text(recevied_message)
 
     message_object = {
         "attachment":{
@@ -49,16 +93,19 @@ def post_facebook_message(fbid, recevied_message):
             "url" : "http://worldversus.com/img/ironman.jpg"
           }
         }
-        }
+    }
 
-    message_object = {
+    message_object2 = {
         "text": joke_text
         }
                    
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message": message_object})
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    response_msg2 = json.dumps({"recipient":{"id":fbid}, "message": message_object})
+    
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
+    
     pprint(status.json())
 
 
@@ -96,7 +143,7 @@ class MyQuoteBotView(generic.View):
 
 
 def index(request):
-    print return_random_quote()
+    print quote_search('z123io90')
     return HttpResponse("Hello World")
 
 def test():
