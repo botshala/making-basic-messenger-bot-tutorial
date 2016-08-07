24d65002baea9ed1f10ad80aecddfb4201d8928e
Saurav Tomar <stomatrix@gmail.com>
2016-08-07 08:43:13 +0530

    fixed an error

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -36,7 +36,7 @@ def post_facebook_message(fbid, recevied_message):
                    
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    response_msg = json.dumps({"recipient":{"id":fbid}, message_object})
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message": message_object} )
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
